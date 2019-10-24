b6bcd2153262c2bcbcf2d924bbe60d33162d793a
simon gao <simon29rock@gmail.com>
2019-10-24 09:30:26 -0400

    analy frag issue, get compile cmd from readme

--- test_pro/frag_test.cc
+++ test_pro/frag_test.cc
@@ -23,42 +23,13 @@ public:
 
 // start frag test
 typedef unsigned int		__u32;
-#include "ceph_frag.h"
 typedef unsigned int		uint32_t;
+#include "ceph_frag.h"
+#include "frag.h"
+/*
+
 typedef uint32_t            _frag_t;
 #define assert(t) if(t == 0) {cout << "assert failed : " << __func__<< endl;}
-/*
-inline uint32_t ceph_frag_make(uint32_t b, uint32_t v)
-{
-	return (b << 24) |
-		(v & (0xffffffu << (24-b)) & 0xffffffu);
-}
-inline uint32_t ceph_frag_make2(uint32_t b, uint32_t v)
-{
-	return 
-		v & (0xffffffu << (24-b));
-}
-inline uint32_t ceph_frag_make3(uint32_t b, uint32_t v)
-{
-	return 
-		(v & (0xffffffu << (24-b)) & 0xffffffu);
-}
-static inline uint32_t ceph_frag_bits(uint32_t f)
-{
-	return f >> 24;
-}
-static inline uint32_t ceph_frag_value(uint32_t f)
-{
-	return f & 0xffffffu;
-}
-
-static inline uint32_t ceph_frag_make_child(uint32_t f, int by, int i)
-{
-	int newbits = ceph_frag_bits(f) + by;
-	return ceph_frag_make(newbits,
-			 ceph_frag_value(f) | (i << (24 - newbits)));
-}
-*/
 class frag_t{
 public:
     _frag_t _enc;
@@ -140,7 +111,137 @@ inline std::ostream& operator<<(std::ostream& out, const frag_t& hb)
   }
   return out << '*';
 }
+*/
+/*
+bool CInode::get_dirfrags_under(frag_t fg, list<CDir*>& ls)
+{
+  bool all = true;
+  std::list<frag_t> fglist;
+  dirfragtree.get_leaves_under(fg, fglist);
+  for (list<frag_t>::iterator p = fglist.begin(); p != fglist.end(); ++p)
+    if (dirfrags.count(*p))
+      ls.push_back(dirfrags[*p]);
+    else 
+      all = false;
 
+  if (all)
+    return all;
+
+  fragtree_t tmpdft;
+  tmpdft.force_to_leaf(g_ceph_context, fg);
+  for (auto &p : dirfrags) {
+    tmpdft.force_to_leaf(g_ceph_context, p.first);
+    if (fg.contains(p.first) && !dirfragtree.is_leaf(p.first))
+      ls.push_back(p.second);
+  }
+
+  all = true;
+  tmpdft.get_leaves_under(fg, fglist);
+  for (const auto &p : fglist) {
+    if (!dirfrags.count(p)) {
+      all = false;
+      break;
+    }
+  }
+
+  return all;
+}
+ */
+void frag_adv(){
+    cout << "--frag_adv--" << endl;
+    list<frag_t> dirfrags;
+    // create fragtree
+    fragtree_t fragtree;
+    frag_t root = frag_t();
+    fragtree.split(root, 3);
+    std::list<frag_t> fglist_lv1;
+    fragtree.get_leaves_under(root, fglist_lv1);
+    auto it = fglist_lv1.begin();
+    ++it;
+    for (; it != fglist_lv1.end();++it){
+        cout << *it << endl;
+        fragtree.split(*it, 3);
+    }
+    fragtree.get_leaves_under(root, dirfrags);
+    cout << "--frag_check--" << endl;
+    frag_t cf = frag_t(262144, 6);  // 000001* 262144 6
+    frag_t cf2 = frag_t(2097152, 6);// 001000* 2097152 6
+    frag_t cf3 = frag_t(0, 3);      // 000* 0 3
+    frag_t cf4 = frag_t(2097152, 3);// 001* 2097152 3
+    frag_t inf = cf4;
+    for (auto &p : dirfrags){
+        cout << p << endl;
+        if (inf == p){
+            cout << inf << " = " << p << endl;
+            break;
+        }
+        else if (inf.bits() < p.bits() && inf.contains(p))
+            cout << inf << " have " << p << endl;
+        else if (inf.bits() > p.bits() && p.contains(inf)){
+            cout << inf << " belong " << p << endl;
+            break;
+        }
+    }
+}
+int frag_test() {
+    cout << "start func : " << __func__ << endl;
+    fragtree_t fragtree;
+    frag_t root = frag_t();
+    fragtree.split(root, 3);   
+    // fragtree.split(root, 3);  // error, root must be leaf
+    cout << fragtree << endl;
+    // test diri->get_dirfrags_under(basefrag, srcfrags);   // get dir under the frag
+    //      |__ dirfragtree.get_leaves_under(fg, fglist);
+    cout << "get leaf : " << root << " and split again"<< endl;
+    std::list<frag_t> fglist_lv1;
+    fragtree.get_leaves_under(root, fglist_lv1);
+    frag_t lv1_1 = *(fglist_lv1.begin());
+    for (auto it = fglist_lv1.begin(); it != fglist_lv1.end();++it){
+        cout << *it << endl;
+        fragtree.split(*it, 3);
+    }
+    cout << fragtree << endl;
+    cout << "get leaf : " << lv1_1 << endl;
+    std::list<frag_t> fglist_lv2;
+    fragtree.get_leaves_under(lv1_1, fglist_lv2);
+    frag_t lv2_1 = *fglist_lv2.begin();
+    for (auto it = fglist_lv2.begin(); it != fglist_lv2.end();++it){
+        cout << *it << " " << it->value() << " " << it->bits()<< endl;
+    }
+    
+    cout << "get leaf : " << lv2_1<< endl;
+    std::list<frag_t> fglist_lv3;
+    fragtree.get_leaves_under(lv2_1, fglist_lv3);
+    for (auto it = fglist_lv3.begin(); it != fglist_lv3.end();++it){
+        cout << *it << " " << it->value() << " " << it->bits()<< endl;
+    }
+    // will get all leaf node
+    // cout << "get leaf : " << root<< endl;
+    // std::list<frag_t> fglist_all_ori;
+    // fragtree.get_leaves_under(root, fglist_all_ori);
+    // for (auto it = fglist_all_ori.begin(); it != fglist_all_ori.end();++it){
+    //    cout << *it << endl;
+    // }
+    cout << "force_to_leaf : " << lv1_1<< endl;
+    fragtree_t tmpdft;
+    tmpdft.force_to_leaf(lv1_1);
+    cout << tmpdft << endl;
+    std::list<frag_t> fglist_all;
+    tmpdft.get_leaves_under(frag_t(), fglist_all);
+    for (auto it = fglist_all.begin(); it != fglist_all.end();++it)
+        cout << *it << " " << it->value() << " " << it->bits()<< endl;
+    fglist_all.clear();
+    cout << "and then force_to_leaf : " << lv2_1<< endl;
+    tmpdft.force_to_leaf(lv2_1);
+    tmpdft.get_leaves_under(frag_t(), fglist_all);
+    for (auto it = fglist_all.begin(); it != fglist_all.end();++it)
+        cout << *it << " " << it->value() << " " << it->bits()<< endl;
+
+    cout << "frag contain test " << lv2_1<< endl;
+    if (lv1_1.contains(lv2_1))
+        cout << " 1v1 have 2_1" << endl;
+    return 0;
+}
 int main(){
     A<int, int> a;
     B<A> b;
@@ -158,25 +259,33 @@ int main(){
             cout << " cnt: " << cnt << " i:" << i << endl;
         }
         }*/
-    //test 2
+    // test null
+    cout << "---- test null" << endl;
     frag_t f = frag_t();
     printf( ".%08llx %i %i\n", (long long unsigned)f, f.value(), f.bits());
+    frag_t f2 = frag_t(2,3);
+    printf( ".%08llx %i %i\n", (long long unsigned)f2, f2.value(), f2.bits());
     cout << f << endl;
+    // test splite 2
+    cout << "---- test split 2 for " << f << endl;
     list<frag_t> l;
     f.split(2,l);
     for (list<frag_t>::iterator it = l.begin(); it != l.end(); ++it){
         printf( ".%08llx %i %i\n", (long long unsigned)*it, (*it).value(), (*it).bits());
         cout << *it << endl;
     }
-    list<frag_t> l2;
+    // test splite 3
     frag_t tmp = *(++l.begin());
+    cout << "test split 3 for " << tmp << endl;
+    list<frag_t> l2;
     tmp.split(3,l2);
     for (list<frag_t>::iterator it = l2.begin(); it != l2.end(); ++it){
         printf( ".%08llx %i %i\n", (long long unsigned)*it, (*it).value(), (*it).bits());
         cout << *it << endl;
     }
 
-    cout << "my test" << endl;
+    // test get parent
+    cout << "---- test get parent for sp. frag" << endl;
     frag_t f_t2 = frag_t(56623104);
     printf( ".%08llx %i %i\n", (long long unsigned)f_t2, f_t2.value(), f_t2.bits());
     cout << f_t2 << endl;
@@ -207,6 +316,8 @@ int main(){
     tmp = ceph_frag_make_child(0, 3, 2);
     printf( ".%08llx %i %i\n", (long long unsigned)tmp, ceph_frag_value(tmp), ceph_frag_bits(tmp));
     */
+    frag_test();
+    frag_adv();
     return 0;
 }
 
